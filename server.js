02349938893d3b0ffbaa1f4699c37d3d7bd41f76
AnshamalaK <anshumala.17@business.mrt.ac.lk>
2020-10-22 18:24:10 +0530

    web app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
-
+//public folder
 app.use(express.static(__dirname + "/public"))
 let clients = 0
 
